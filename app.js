a1d0046a9859a236923ae295366919dc45081e73
deekonda-rohan <rohandeekonda007@gmail.com>
2026-08-29 11:48:16 +0530

    login

--- app.js
+++ app.js
@@ -4,50 +4,67 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import path from "path";
 import { fileURLToPath } from "url";
-import userModel from "./models/user.model.js"
+import userModel from "./models/user.model.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/',(req,res)=>{
-    res.render("index");
-})
+app.post("/create", async (req, res) => {
+  const { name, age, email, password } = req.body;
 
-app.post('/create',async (req,res)=>{
-    const {name,age,email,password} = req.body;
+  const hashed = await bcrypt.hash(password, 10);
 
-    const hashed = await bcrypt.hash(password,10);
+  await userModel.create({
+    name,
+    email,
+    age,
+    password: hashed,
+  });
 
-    await userModel.create({
-        name,
-        email,
-        age,
-        password : hashed,
-    })
+  const token = jwt.sign({ email }, "sahfajghsafhjhajasdfj");
+  res.cookie("token", token);
 
-    const token = jwt.sign({email},sahfajghsafhjhajasdfj);
-    res.cookie("token",token)
+  res.redirect("/login");
+});
 
-    res.redirect("/login");
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.get('/login',(req,res)=>{
-    res.redirect('login');
-})
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-app.post('/login',(req,res)=>{
-    
-})
+  const user = await userModel.findOne({ email });
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+  if (!user) {
+    return res.send("Invalid Email");
+  }
+
+  const result = await bcrypt.compare(password, user.password);
+
+  if (!result) {
+    return res.send("Invalid Password");
+  }
+
+  const token = jwt.sign({ email }, "sahfajghsafhjhajasdfj");
+
+  res.cookie("token", token);
+
+  res.send("Login Successful");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
